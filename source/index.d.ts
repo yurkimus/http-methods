a37b9b25169458d631140d5861773e115c396e50
Iurii <yurkimus@gmail.com>
2024-08-10 20:35:36 +0200

    Add package.json types and generate them

---
+++ source/index.d.ts
@@ -0,0 +1,12 @@
+declare module "index" {
+    export namespace HttpMethods {
+        let Get: "GET";
+        let Head: "HEAD";
+        let Post: "POST";
+        let Put: "PUT";
+        let Delete: "DELETE";
+        let Connect: "CONNECT";
+        let Options: "OPTIONS";
+        let Trace: "TRACE";
+    }
+}
